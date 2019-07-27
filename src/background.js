82d69e06573b1fc96d14262ec5820fe5d244d878
serj.kzv <serj.kzv@gmail.com>
2019-07-27 07:58:30 +0300

    dev

--- src/background.js
+++ src/background.js
@@ -3,11 +3,7 @@
 let timer;
 
 browser.browserAction.onClicked.addListener(async () => {
-    const downloads = await browser.downloads.search({
-        limit: 1,
-        orderBy: ['-startTime'],
-        state: 'complete'
-    });
+    const downloads = await browser.downloads.search({limit: 1, orderBy: ['-startTime'], state: 'complete'});
 
     if (downloads.length > 0) {
         navigator.clipboard.writeText(downloads[0].url);
